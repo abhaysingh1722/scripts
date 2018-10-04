7caf5c652fb8420720782c803c13c771bce4817a
Siddesh Sangodkar <sid226@gmail.com>
2018-10-04 10:15:24 +0000

    fixed sles issue

--- Go/install.sh
+++ Go/install.sh
@@ -165,7 +165,7 @@ case "$DISTRO" in
   configureAndInstall
   ;;
 
-"SLES-15")
+"sles-15")
   printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" | tee -a "$LOG_FILE"
   sudo zypper install -y tar wget gcc
   configureAndInstall
